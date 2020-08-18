fdc1f0f68ef040e00f84bf0b69754382a330520a
Andrei Blaj <andreiblajdev@gmail.com>
2020-08-17 20:52:54 -0400

    List, NavigationView, NavigationLink

---
+++ Landmarks/Landmarks/Supporting Views/CircleImage.swift
@@ -0,0 +1,27 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Andrei Blaj on 8/17/20.
+//  Copyright © 2020 Andrei Blaj. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    
+    var body: some View {
+        image
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.gray, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage(image: landmarkData[0].image)
+    }
+}
---
+++ Landmarks/Landmarks/Supporting Views/LandmarkList.swift
@@ -0,0 +1,33 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Andrei Blaj on 8/17/20.
+//  Copyright © 2020 Andrei Blaj. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+//        LandmarkList()
+//            .previewDevice(PreviewDevice(rawValue: "iPhone SE (1st generation)"))
+        ForEach(["iPhone SE (1st generation)", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+        }
+    }
+}
